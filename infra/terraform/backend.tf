606c6badafc2fb0fa6c859fc302fe55e1b6f4cd1
emmanuel <emmalux2@gmail.com>
2026-08-27 11:32:59 +0100

    Fix Terraform S3 state bucket

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "secure-cloud-microservices-tfstate-797776210271"
+    bucket       = "my-terraform-state-ansy"
     key          = "secure-cloud-microservices/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
