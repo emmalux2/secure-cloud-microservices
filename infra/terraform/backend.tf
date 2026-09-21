d33cfeda6465b1878838e874805dd1f386af0c43
emmanuel <emmalux2@gmail.com>
2026-09-21 20:59:52 +0100

    message

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "secure-cloud-microservices-tfstate-797776210271"
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "secure-cloud-tf-locks" # or use_lockfile = true if you want to drop DynamoDB
-    encrypt        = true
+    bucket       = "secure-cloud-microservices-tfstate-797776210271"
+    key          = "eks/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
